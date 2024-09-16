11e716243e053515c36a7500e5cdd7aa9046988c
T.Suzuki@MBP <st09d18@icloud.com>
2024-09-16 21:22:04 +0900

    Scaffold試してみる

--- src/models/_entities/posts.rs
+++ src/models/_entities/posts.rs
@@ -4,13 +4,14 @@ use sea_orm::entity::prelude::*;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq, Serialize, Deserialize)]
-#[sea_orm(table_name = "notes")]
+#[sea_orm(table_name = "posts")]
 pub struct Model {
     pub created_at: DateTimeWithTimeZone,
     pub updated_at: DateTimeWithTimeZone,
     #[sea_orm(primary_key)]
     pub id: i32,
     pub title: Option<String>,
+    #[sea_orm(column_type = "Text", nullable)]
     pub content: Option<String>,
 }
 
